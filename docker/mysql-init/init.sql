667a307bfd417657c9eade4a81cee20de90bf945
Stefan Konno <stefan.konno@schibsted.com>
2020-08-14 09:40:17 +0200

    New data structure.

--- docker/mysql-init/init.sql
+++ docker/mysql-init/init.sql
@@ -2,11 +2,24 @@ CREATE DATABASE chat;
 CREATE USER 'chat_user'@'%' IDENTIFIED WITH mysql_native_password BY 'supersecret';
 GRANT SELECT, INSERT, UPDATE, DELETE ON chat.* TO 'chat_user'@'%';
 
-CREATE TABLE chat.subscribers (
-account VARCHAR(50) NOT NULL,
-chat VARCHAR(15) NOT NULL,
+CREATE TABLE chat.accounts (
+id VARCHAR(50) PRIMARY KEY,
 online BOOLEAN NOT NULL DEFAULT TRUE,
-banned BOOLEAN NOT NULL DEFAULT FALSE,
+moderator BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE chat.bans (
+account_id VARCHAR(50),
+chat VARCHAR(15) NOT NULL,
+expires_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY(account_id, chat),
+FOREIGN KEY (account_id) REFERENCES accounts(id) ON DELETE CASCADE
+);
+
+CREATE TABLE chat.subscribers (
+account_id VARCHAR(50) NOT NULL,
+chat VARCHAR(15) NOT NULL,
 subscribed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-PRIMARY KEY(account, chat)
-)
+PRIMARY KEY(account_id, chat),
+FOREIGN KEY (account_id) REFERENCES accounts(id) ON DELETE CASCADE
+);
